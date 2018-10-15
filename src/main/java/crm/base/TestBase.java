0206a60a5bd7d7221a6267a1dc9843c51b529723
Daroo84 <darekstaron@gmail.com>
2018-10-15 18:39:01 +0200

    Added TestBase class

--- src/main/java/crm/base/TestBase.java
+++ src/main/java/crm/base/TestBase.java
@@ -2,13 +2,55 @@ package crm.base;
 
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
     static WebDriver driver;
     static Properties prop;
+
+    public TestBase(){
+        try {
+            prop = new Properties();
+            FileInputStream ip = new FileInputStream(System.getProperty("user.dir") + "/");
+
+            prop.load(ip);
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void initialization(){
+        String browserName = prop.getProperty("browser");
+
+        if(browserName.equals("chrome")){
+            System.setProperty("", "");
+            driver =new ChromeDriver();
+        }
+        else if (browserName.equals("FF")){
+            System.setProperty("", "");
+            driver = new FirefoxDriver();
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+        driver.get(prop.getProperty("url"));
+    }
 
 
 }
