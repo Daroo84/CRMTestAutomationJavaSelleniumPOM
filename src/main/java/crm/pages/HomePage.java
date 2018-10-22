7d41753b5991dfbd870ddea8f662ecad6b8bd457
Daroo84 <darekstaron@gmail.com>
2018-10-22 16:06:39 +0200

    Added HomePage class

--- src/main/java/crm/pages/HomePage.java
+++ src/main/java/crm/pages/HomePage.java
@@ -1,7 +1,31 @@
 package crm.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static crm.base.TestBase.driver;
+
 public class HomePage {
 
 
-    
-}
+    @FindBy(xpath ="")
+    WebElement userNameLabel;
+
+    @FindBy(xpath = "")
+    WebElement contactsLink;
+
+    @FindBy(xpath = "")
+    WebElement dealsLink;
+
+    @FindBy(xpath = "")
+    WebElement tasksLink;
+
+    //Initializing the Page Object
+
+
+    public HomePage(){
+        PageFactory.initElements(driver,this);
+
+    }
+    public
