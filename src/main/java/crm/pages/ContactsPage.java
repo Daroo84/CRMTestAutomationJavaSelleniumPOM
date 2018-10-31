d299d3b56648b4ee3e922adbcdb7767c66021f25
Daroo84 <darekstaron@gmail.com>
2018-10-31 13:28:29 +0100

    Added ContactsPage some fixes

--- src/main/java/crm/pages/ContactsPage.java
+++ src/main/java/crm/pages/ContactsPage.java
@@ -1,6 +1,7 @@
 package crm.pages;
 
 import crm.base.TestBase;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -17,7 +18,9 @@ public class ContactsPage extends TestBase {
     public boolean verifyContactsLabel(){
         return contactsLabel.isDisplayed();
     }
-    public void selectContactsByName(String)
+    public void selectContactsByName(String name){
+        driver.findElement(By.xpath("//a[text()='"+name+"'//parent::td[@class='datalistrow']" + "//preceding-sibling::td[@class='datalistrow']//input[@name='contact_id']")).click();
+    }
 
 
 }
