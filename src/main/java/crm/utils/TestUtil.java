596960afe8034192a4add898638e38882852cd12
Daroo84 <darekstaron@gmail.com>
2018-11-16 11:44:53 +0100

    Fix TestUtil.java

--- src/main/java/crm/utils/TestUtil.java
+++ src/main/java/crm/utils/TestUtil.java
@@ -4,14 +4,18 @@ import crm.base.TestBase;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import org.apache.commons.io.FileUtils;
 
 public class TestUtil extends TestBase {
 
@@ -28,7 +32,14 @@ public class TestUtil extends TestBase {
     public void switchToFrame(){
         driver.switchTo().frame("mainpanel");
 
+
     }
+    public static void takeScreenshotAtEndOfTest()throws IOException{
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        String currentDir = System.getProperty("user.dir");
+        FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
+    }
+
     public static Object[][] getTestData(String sheetName) {
         FileInputStream file = null;
         try {
